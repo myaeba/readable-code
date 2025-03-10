10d2e5ab640b9de57bf0ae54c65c740765b44168
myaeba <lim104321@naver.com>
2025-03-10 11:21:11 +0900

    [3 -5] Mineswweper 분리, GameApplication이름 변경

---
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -0,0 +1,14 @@
+package cleancode.minesweeper.tobe;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class GameApplication {
+
+    public static void main(String[] args) {
+        Mineswepper mineswepper = new Mineswepper();
+        mineswepper.run();
+    }
+
+}
